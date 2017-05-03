3944ef16c45b65a955fb0b768ce06570aff3f6a7
Unknown <leo.castanie@gmail.com>
2017-05-03 16:36:06 +0200

    Revert "Revert "ajout class turn_manager""
    This reverts commit 308fc91dcb9c9f474dee8feb32f0cf4a1dc0f85d.

---
+++ class/turn.class.php
@@ -0,0 +1,32 @@
+<?php 
+
+/**
+* 
+*/
+class TurnManager 
+{
+	private $_pdo;
+	private $_t_history;
+	
+	private function __construct($pdo)
+	{
+		
+	}
+
+	private function setHistory() 
+	{
+
+	}
+	private function getHistory() 
+	{
+
+	}
+
+
+
+}
+
+
+
+
+?>
